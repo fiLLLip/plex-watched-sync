7c9838ab2db834f55b0c195712459c8f783a31c7
fiLLLip <filiptomren@gmail.com>
2016-07-09 15:20:43 +0200

    Added correctly formatted CORS for frontend

--- server/middleware.py
+++ server/middleware.py
@@ -1,3 +1,6 @@
 class Crossdomain(object):
     def process_response(self, req, resp, resource):
-        resp.set_header('Access-Control-Allow-Origin', '*')
+        resp.set_header("Access-Control-Allow-Origin", "*")
+        resp.set_header("Access-Control-Allow-Credentials", "true")
+        resp.set_header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
+        resp.set_header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, X-token")
